b898e933b94f6e81275d013c223c418ad42b2793
Yangjie Gu <guyjwork@hotmail.com>
2021-09-29 09:24:28 +0800

    Add ListReview.

--- c_data_struct/13_ListReview/DLinkList2.0/DLinkList.c
+++ c_data_struct/13_ListReview/DLinkList2.0/DLinkList.c
@@ -17,10 +17,10 @@ DLinkList *DLinkList_Create() // O(1)
 
     if (ret != NULL)
     {
+        ret->length = 0;
         ret->header.next = NULL;
         ret->header.pre = NULL;
         ret->slider = NULL;
-        ret->length = 0;
     }
 
     return ret;
@@ -37,10 +37,10 @@ void DLinkList_Clear(DLinkList *list) // O(1)
 
     if (sList != NULL)
     {
+        sList->length = 0;
         sList->header.next = NULL;
         sList->header.pre = NULL;
         sList->slider = NULL;
-        sList->length = 0;
     }
 }
 
@@ -87,8 +87,12 @@ int DLinkList_Insert(DLinkList *list, DLinkListNode *node, int pos) // O(n)
 
         if (sList->length == 0)
         {
-            node->pre = NULL;
             sList->slider = node;
+        }
+
+        if (current == (DLinkListNode *)sList)
+        {
+            node->pre = NULL;
         }
 
         sList->length++;
